4c34747982fd4cb0d47fb1659199ed56bff63008
administroot <1474668090@qq.com>
2024-12-24 21:55:40 +0800

    vault backup: 2024-12-24 21:55:40

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,19 @@ pub fn main() -> iced::Result {
 
 enum MyAppMessage {
     Update3(String),
+    Update4(String),
 }
 
 #[derive(Default)]
 struct State{
     text3: String,
+    text4: String,
 }
 
 fn update(state: &mut State, message: MyAppMessage) {
     match message {
         MyAppMessage::Update3(s) => {state.text3 = s;}
+        MyAppMessage::Update4(s) => {state.text4 = s;}
     }
 }
 
@@ -27,6 +30,7 @@ fn view(state: &State) -> Column<MyAppMessage> {
         TextInput::new("Construct from struct", ""),
         text_input("Enabled text input", state.text3.as_str())
             .on_input(|s| MyAppMessage::Update3(s)),
+        text_input("Shorter on_input", state.text4.as_str()).on_input(MyAppMessage::Update4),
     ]
     .into()
 }
