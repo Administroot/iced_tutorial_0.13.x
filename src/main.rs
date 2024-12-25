c70635628ff7f37893c48326d40af6ec35dfeb6d
administroot <1474668090@qq.com>
2024-12-25 13:14:38 +0800

    vault backup: 2024-12-25 13:14:38

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,13 @@ pub fn main() -> iced::Result {
 enum MyAppMessage {
     DoNothing,
     Update3(bool),
+    Update4(bool),
 }
 
 #[derive(Default)]
 struct State {
     toggler3: bool,
+    toggler4: bool,
 }
 
 fn update(state: &mut State, message: MyAppMessage) {
@@ -21,7 +23,10 @@ fn update(state: &mut State, message: MyAppMessage) {
         MyAppMessage::DoNothing => {},
         MyAppMessage::Update3(b) => {
             state.toggler3 = b;
-        }
+        },
+        MyAppMessage::Update4(b) => {
+            state.toggler4 = b;
+        },
     }
 }
 
@@ -29,6 +34,8 @@ fn view(state: &State) -> Column<MyAppMessage> {
     column![
         Toggler::new(false).label("Construct from struct").on_toggle(|_| MyAppMessage::DoNothing),
         toggler(false).label("Construct from function").on_toggle(|_| MyAppMessage::DoNothing),
-        toggler(state.toggler3).label("Functional toggler").on_toggle(|b| MyAppMessage::Update3(b))
+        toggler(state.toggler3).label("Functional toggler").on_toggle(|b| MyAppMessage::Update3(b)),
+        toggler(state.toggler4).label("Shorter parameter").on_toggle(MyAppMessage::Update4),
+        toggler(false).label("Larger button").on_toggle(|_| MyAppMessage::DoNothing).size(30),
     ]
 }
