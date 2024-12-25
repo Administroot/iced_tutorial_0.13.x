1f6b804d9296a483b5cdebe2a3caa77a8dec98a6
administroot <1474668090@qq.com>
2024-12-25 16:34:28 +0800

    vault backup: 2024-12-25 16:34:28

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use iced::widget::{column, Column};
+use iced::widget::{column, Column, pick_list, PickList};
 
 pub fn main() -> iced::Result {
     iced::application("My app", update, view).run()
@@ -7,16 +7,43 @@ pub fn main() -> iced::Result {
 #[derive(Debug, Clone)]
 
 enum MyAppMessage {
-    _Increment,
+    DoNothing,
+    Update3(String),
 }
 
 #[derive(Default)]
 struct State {
-    _checkbox4: bool,
+    pick_list_3: Option<String>, 
 }
 
-fn update(_state: &mut State, _message: MyAppMessage) {}
+fn update(state: &mut State, message: MyAppMessage) {
+    match message {
+        MyAppMessage::DoNothing => {},
+        MyAppMessage::Update3(s) => {
+            state.pick_list_3 = Some(s);
+        }
+    }
+}
 
-fn view(_state: &State) -> Column<MyAppMessage> {
-    column!["hello, world"].into()
+fn view(state: &State) -> Column<MyAppMessage> {
+    column![
+        PickList::new(
+            vec!["Construct from struct"],
+            Some("Construct from struct"),
+            |_| MyAppMessage::DoNothing
+        ),
+        pick_list(
+            vec!["Construct from function"],
+            Some("Construct from function"),
+            |_| MyAppMessage::DoNothing
+        ),
+        pick_list(
+            ["Functional pick list", "Other choices 1", "Other choices 2"]
+                .map(|s| s.to_string())
+                .to_vec(),
+            S
+            state.pick_list_3.clone(),
+            |s| MyAppMessage::Update3(s)
+        ),
+    ].into()
 }
