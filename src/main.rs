88398ed602bc2744a1d46f4fabf1ab72599e43d7
administroot <1474668090@qq.com>
2024-12-25 16:44:29 +0800

    vault backup: 2024-12-25 16:44:29

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use iced::widget::{column, Column, pick_list, PickList};
+use iced::{widget::{column, Column, pick_list, PickList, text::Shaping, pick_list::Handle}, font::Family, Font};
 
 pub fn main() -> iced::Result {
     iced::application("My app", update, view).run()
@@ -37,13 +37,45 @@ fn view(state: &State) -> Column<MyAppMessage> {
             Some("Construct from function"),
             |_| MyAppMessage::DoNothing
         ),
+        // A typical pick list
         pick_list(
-            ["Functional pick list", "Other choices 1", "Other choices 2"]
+            ["Other choices 1", "Other choices 2"]
                 .map(|s| s.to_string())
                 .to_vec(),
-            S
             state.pick_list_3.clone(),
             |s| MyAppMessage::Update3(s)
-        ),
+        ).placeholder("Functional pick list"),
+        pick_list(vec!["A", "B", "C"], None::<&str>, |_| {
+            MyAppMessage::DoNothing
+        })
+        .placeholder("Placeholder"),
+        // Personalization
+        pick_list(vec!["Different font"], Some("Different font"), |_| {
+            MyAppMessage::DoNothing
+        })
+        .font(Font {
+            family: Family::Fantasy,
+            ..Font::DEFAULT
+        }),
+        pick_list(vec!["Larger text"], Some("Larger text"), |_| {
+            MyAppMessage::DoNothing
+        })
+        .text_size(24),
+        pick_list(
+            vec!["Special character 😊"],
+            Some("Special character 😊"),
+            |_| MyAppMessage::DoNothing
+        )
+        .text_shaping(Shaping::Advanced),
+        pick_list(vec!["With padding"], Some("With padding"), |_| {
+            MyAppMessage::DoNothing
+        })
+        .padding(20),
+        pick_list(vec!["Different handle"], Some("Different handle"), |_| {
+            MyAppMessage::DoNothing
+        })
+        .handle(Handle::Arrow {
+            size: Some(Pixels(24.))
+        }),
     ].into()
 }
