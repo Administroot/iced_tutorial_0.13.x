97cf9dcf2344e1dc7421cd9d14e1f3111c3c5d1c
administroot <1474668090@qq.com>
2024-12-25 12:54:28 +0800

    vault backup: 2024-12-25 12:54:28

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use iced::widget::{column, Column};
+use iced::widget::{column, Column, Toggler, toggler};
 
 pub fn main() -> iced::Result {
     iced::application("My app", update, view).run()
@@ -18,5 +18,9 @@ struct State {
 fn update(_value: &mut u64, _message: MyAppMessage) {}
 
 fn view(_value: &u64) -> Column<MyAppMessage> {
-    column!["hello, world"]
+    column![
+        Toggler::new(Some("Construct from struct".into()), false, |_| {
+            MyAppMessage::DoNothing
+        }),
+    ]
 }
