12e3a865ad7c75f39a191eb79c43f1d474d65b40
administroot <1474668090@qq.com>
2024-12-25 13:29:47 +0800

    vault backup: 2024-12-25 13:29:47

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use iced::{widget::{column, toggler, Column, Toggler, text::Shaping}, Font, font::Family};
+use iced::{
+    alignment::Horizontal, font::Family, widget::{column, text::Shaping, toggler, Column, Toggler}, Font
+};
 
 pub fn main() -> iced::Result {
     iced::application("My app", update, view).run()
@@ -20,30 +22,50 @@ struct State {
 
 fn update(state: &mut State, message: MyAppMessage) {
     match message {
-        MyAppMessage::DoNothing => {},
+        MyAppMessage::DoNothing => {}
         MyAppMessage::Update3(b) => {
             state.toggler3 = b;
-        },
+        }
         MyAppMessage::Update4(b) => {
             state.toggler4 = b;
-        },
+        }
     }
 }
 
 fn view(state: &State) -> Column<MyAppMessage> {
     column![
-        Toggler::new(false).label("Construct from struct").on_toggle(|_| MyAppMessage::DoNothing),
-        toggler(false).label("Construct from function").on_toggle(|_| MyAppMessage::DoNothing),
-        toggler(state.toggler3).label("Functional toggler").on_toggle(|b| MyAppMessage::Update3(b)),
-        toggler(state.toggler4).label("Shorter parameter").on_toggle(MyAppMessage::Update4),
-        toggler(false).label("Larger button").on_toggle(|_| MyAppMessage::DoNothing).size(30),
-        toggler(false).label("Different font").on_toggle(|_| MyAppMessage::DoNothing).font(
-            Font{
+        Toggler::new(false)
+            .label("Construct from struct")
+            .on_toggle(|_| MyAppMessage::DoNothing),
+        toggler(false)
+            .label("Construct from function")
+            .on_toggle(|_| MyAppMessage::DoNothing),
+        toggler(state.toggler3)
+            .label("Functional toggler")
+            .on_toggle(|b| MyAppMessage::Update3(b)),
+        toggler(state.toggler4)
+            .label("Shorter parameter")
+            .on_toggle(MyAppMessage::Update4),
+        toggler(false)
+            .label("Larger button")
+            .on_toggle(|_| MyAppMessage::DoNothing)
+            .size(30),
+        toggler(false)
+            .label("Different font")
+            .on_toggle(|_| MyAppMessage::DoNothing)
+            .font(Font {
                 family: Family::Fantasy,
                 ..Font::DEFAULT
-            }
-        ),
-        toggler(false).label("Larger text").on_toggle(|_| MyAppMessage::DoNothing).text_size(24),
-        toggler(false).label("Special character 😊").on_toggle(|_| MyAppMessage::DoNothing).text_shaping(Shaping::Advanced),
+            }),
+        toggler(false)
+            .label("Larger text")
+            .on_toggle(|_| MyAppMessage::DoNothing)
+            .text_size(24),
+        toggler(false)
+            .label("Special character 😊")
+            .on_toggle(|_| MyAppMessage::DoNothing)
+            .text_shaping(Shaping::Advanced),
+        toggler(false).label("Space between button and text").on_toggle(|_| MyAppMessage::DoNothing).spacing(30),
+        toggler(false).label("Centered text").on_toggle(|_| MyAppMessage::DoNothing).Center,
     ]
 }
