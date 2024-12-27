90ae66c7390a57613cb00d2a6e8abbb6058f5cf4
administroot <1474668090@qq.com>
2024-12-27 22:15:04 +0800

    vault backup: 2024-12-27 22:15:04

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 This widget is composed by a TextInput that can be filled with the text to search
 for corresponding values from the list of options that are displayed as a Menu. */
 
-use iced::{widget::{column, combo_box, combo_box::State, ComboBox, scrollable}, Element};
+use iced::{widget::{column, combo_box, combo_box::State, ComboBox, scrollable, text}, Element};
 
 fn main() -> iced::Result {
     iced::run("ComboBox", MyApp::update, MyApp::view)
@@ -14,9 +14,12 @@ struct MyApp {
     state3: State<Words>,
     my_words: State<Words>,
     state5: State<Words>,
+    state6: State<Words>,
+    input6: String,
     
     selected_word: Option<Words>, 
     selected5: Option<Words>,
+    selected6: Option<Words>,
 }
 
 impl Default for MyApp {
@@ -30,6 +33,8 @@ enum Message {
     Selected(Words),
     DoNothing,
     Selected5(Words),
+    Select6(Words),
+    Input6(String),
 }
 
 #[derive(Debug, Clone, Copy, Default)]
@@ -66,6 +71,7 @@ impl MyApp {
             state2: State::new(vec![]),
             state3: State::new(Words::ALL.to_vec()),
             state5: State::new(Words::ALL.to_vec()),
+            input6: "".into(),
             selected_word: None,
             selected5: None,
         }
@@ -79,6 +85,12 @@ impl MyApp {
             Message::DoNothing => {},
             Message::Selected5(w) => {
                 self.selected5 = Some(w);
+            },
+            Message::Select6(w) => {
+                self.selected6 = Some(w);
+            },
+            Message::Input6(s) => {
+                self.input6 = Some(s);
             }
         }
     }
@@ -114,12 +126,21 @@ impl MyApp {
             self.selected5.as_ref(),
             Message::Selected5,
         );
+        let combo_box_6 = combo_box(
+            &self.state6,
+            "Respond to input",
+            self.selected6.as_ref(),
+            Message::Select6
+        )
+        .on_input(Message::Input6);
         let content = column![
             combo_box_1,
             combo_box_2,
             combo_box_3,
             combo_box_4,
             combo_box_5,
+            text(&self.input6),
+            combo_box_6,
         ];
         scrollable(content).into()
     }
