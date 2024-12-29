3fe63a294c3574b76f27102cd5614ac42561697c
administroot <1474668090@qq.com>
2024-12-29 14:32:53 +0800

    vault backup: 2024-12-29 14:32:53

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,10 @@ struct MyApp {
     value3: u32,
     value4: u32,
     value5: u32,
+    value6: u32,
+    value7: u32,
+    value8: u32,
+    released_value_7: u32,
 }
 
 impl Default for MyApp {
@@ -26,6 +30,11 @@ enum MyAppMessage {
     DoNothing,
     Update3(u32),
     Update4(u32),
+    Update5(u32),
+    Update6(u32),
+    Update7(u32),
+    
+    Release7,
 }
 
 impl MyApp {
@@ -33,6 +42,10 @@ impl MyApp {
         Self {
             value3: 50,
             value4: 50,
+            value5: 50,
+            value6: 50,
+            value7: 50,
+            released_value_7: 50,
         }
     }
 
@@ -44,6 +57,18 @@ impl MyApp {
             },
             MyAppMessage::Update4(v) => {
                 self.value4 = v;
+            },
+            MyAppMessage::Update5(v) => {
+                self.value5 = v;
+            },
+            MyAppMessage::Update6(v) => {
+                self.value6 = v;
+            },
+            MyAppMessage::Update7(v) => {
+                self.value7 = v;
+            },
+            MyAppMessage::Release7 => {
+                self.released_value_7 = self.value7;
             }
         }
     }
@@ -59,7 +84,13 @@ impl MyApp {
             text("Shorter parameter"),
             slider(0..=100, self.value4, MyAppMessage::Update4),
             text("Different step"),
-            slider(0..=100, , on_change)
+            slider(0..=100, self.value5, MyAppMessage::Update5).step(10u32),
+            text("Different step when a shift key is pressed"),
+            slider(0..=100, self.value6, MyAppMessage::Update6).shift_step(10u32),
+            text(format!("React to mouse release: {}", self.released_value_7)),
+            slider(0..=100, self.value7, MyAppMessage::Update7).on_release(MyAppMessage::Release7),
+            text("Press Ctrl (or Command) and click to return to the default value"),
+            slider(0..=100, self.value8, MyAppMessage::Update8).default(30u32),
         ).into()
     }
 }
