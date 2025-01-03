e8ad7f7e1f6ecd29d9245b19c7ca8ca2e5f2746a
administroot <1474668090@qq.com>
2025-01-03 13:03:32 +0800

    vault backup: 2025-01-03 13:03:32

--- src/main.rs
+++ src/main.rs
@@ -36,28 +36,7 @@ impl MyApp {
             radio("Choice A", "A", Some("A"), |s| Message::Choose(
                 s.to_string()
             ))
-            .style(Theme::Custom(Box::new(RadioStyle)))
+            .style(style::Radio::new(Color::BLACK))
         ].into()
-    }
-}
-
-struct RadioStyle;
-
-impl radio::StyleSheet for RadioStyle {
-    type Style = iced::Theme;
-
-    fn active(&self, style: &Self::Style, is_selected: bool) -> radio::Appearance {
-        radio::Style {
-            text_color: Some(if is_selected {
-                Color::from_rgb(0., 0., 1.)
-            } else {
-                Color::from_rgb(0.5, 0.5, 0.5)
-            }),
-            ..style.active(&Theme::Default, is_selected)
-        }
-    }
-
-    fn hovered(&self, style: &Self::Style, is_selected: bool) -> radio::Appearance {
-        style.hovered(&Theme::Default, is_selected)
     }
 }
