f94feb2be760f81fb2f6bd95eef766d425d05663
administroot <1474668090@qq.com>
2025-01-06 15:56:00 +0800

    Passing_parameters_across_pages

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use iced::{
-    widget::{column, text},
+    widget::{button, column, text, text_input},
     Element,
 };
 
 fn main() -> iced::Result {
-    iced::application("My First App", MyApp::update, MyApp::view).run()
+    iced::application("My App", MyApp::update, MyApp::view).run()
 }
 
 struct MyApp {
-    _state: String,
+    page: Box<dyn Page>,
 }
 
 impl Default for MyApp {
@@ -17,23 +17,115 @@ impl Default for MyApp {
     }
 }
 
+// Page B
+#[derive(Debug, Clone)]
+enum PageBMessage {
+    ButtonPressed,
+}
+
+type Mb = PageBMessage;
+
+struct PageB {
+    name: String
+}
+
+impl PageB {
+    fn new(name: String) -> Self {
+        Self { name }
+    }
+}
+
+impl Page for PageB {
+    fn update(&mut self, message: Message) -> Option<Box<dyn Page>> {
+        if let Message::PageB(msg) = message {
+            match msg {
+                PageBMessage::ButtonPressed => return Some(Box::new(PageA::new())),
+            }
+        }
+        None
+    }
+
+    fn view(&self) -> iced::Element<Message> {
+        column![
+            text(format!("hello, {}!", self.name)),
+            button("Log out").on_press(Message::PageB(Mb::ButtonPressed)),
+        ]
+        .into()
+    }
+}
+
+// Page A
+#[derive(Debug, Clone)]
+enum PageAMessage {
+    TextChanged(String),
+    ButtonPressed,
+}
+
+type Ma = PageAMessage;
+
+struct PageA {
+    name: String,
+}
+
+impl PageA {
+    fn new() -> Self {
+        PageA {
+            name: String::new(),
+        }
+    }
+}
+
+impl Page for PageA {
+    fn update(&mut self, message: Message) -> Option<Box<dyn Page>> {
+        if let Message::PageA(msg) = message {
+            match msg {
+                PageAMessage::TextChanged(s) => self.name = s,
+                PageAMessage::ButtonPressed => {
+                    // Here, passing parameter name across pages, from A to B
+                    return Some(Box::new(PageB::new(self.name.clone())));
+                }
+            }
+        }
+        None
+    }
+
+    fn view(&self) -> iced::Element<Message> {
+        column![
+            text_input("Name", &self.name)
+                .secure(true)
+                .on_input(|s| Message::PageA(Ma::TextChanged(s))),
+            button("Log in").on_press(Message::PageA(Ma::ButtonPressed)),
+        ]
+        .into()
+    }
+}
+
 #[derive(Debug, Clone)]
 enum Message {
-    _Message1,
+    PageA(PageAMessage),
+    PageB(PageBMessage),
+}
+
+trait Page {
+    fn update(&mut self, message: Message) -> Option<Box<dyn Page>>;
+    fn view(&self) -> iced::Element<'_, Message>;
 }
 
 impl MyApp {
     fn new() -> Self {
         Self {
-            _state: String::new(),
+            page: Box::new(PageA::new()),
         }
     }
 
-    fn update(&mut self, _message: Message) {
-        todo!()
+    fn update(&mut self, message: Message) {
+        let page = self.page.update(message);
+        if let Some(p) = page {
+            self.page = p;
+        }
     }
 
     fn view(&self) -> Element<Message> {
-        column!(text("Hello World!".to_string()),).into()
+        self.page.view()
     }
 }
