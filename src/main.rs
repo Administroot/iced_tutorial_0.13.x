1579c8ffe19513c2e1f8f39939608fc5ac2c8090
administroot <1474668090@qq.com>
2025-01-07 13:20:40 +0800

    vault backup: 2025-01-07 13:20:40

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,61 @@
 use iced::{
-    widget::{column, text},
-    Element,
+    widget::{button, column, text_input}, Element, Task,
 };
 
+const MY_TEXT_ID: &str = "my_text";
+
 fn main() -> iced::Result {
-    iced::application("My First App", MyApp::update, MyApp::view).run()
+    iced::application(
+        "controlling widgets by commands",
+        MyApp::update,
+        MyApp::view,
+    )
+    .run()
 }
 
 struct MyApp {
-    _state: String,
+    some_text: String,
 }
 
 impl Default for MyApp {
     fn default() -> Self {
-        MyApp::new()
+        MyApp::new().0
     }
 }
 
 #[derive(Debug, Clone)]
 enum Message {
-    _Message1,
+    UpdateText(String),
+    SelectAll,
 }
 
 impl MyApp {
-    fn new() -> Self {
-        Self {
-            _state: String::new(),
-        }
+    fn new() -> (Self, Task<Message>) {
+        (
+            Self {
+                some_text: String::new(),
+            },
+            Task::none(),
+        )
     }
 
-    fn update(&mut self, _message: Message) {
-        todo!()
+    fn update(&mut self, message: Message) -> Task<Message> {
+        match message {
+            Message::UpdateText(s) => self.some_text = s,
+            Message::SelectAll => {}
+        }
+        Task::none()
     }
 
     fn view(&self) -> Element<Message> {
-        column!(text("Hello World!".to_string()),).into()
+        column!(
+            // button("Edit text").on_press(Message::EditText),
+            text_input("", &self.some_text)
+                // Sets the Id of the TextInput.
+                .id(text_input::Id::new(MY_TEXT_ID))
+                .on_input(Message::UpdateText),
+            button("Select all").on_press(Message::)
+        )
+        .into()
     }
 }
