9c4d4665504e353d224b8436421ca3a8d71a5776
administroot <1474668090@qq.com>
2025-01-09 14:55:24 +0800

    vault backup: 2025-01-09 14:55:24

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,47 @@
 use iced::{
-    advanced::{Widget, widget, Layout, Renderer}, 
-    Length, Theme, Size
+    advanced::{layout, renderer::{self, Quad}, widget, Layout, Widget}, mouse, Border, Color, Element, Length, Rectangle, Shadow, Size, Theme
 };
 
 fn main() -> iced::Result {
-    iced::application("drawing with caches", MyApp::update, MyApp::view).run()
+    iced::application("My First App", MyApp::update, MyApp::view).run()
+}
+
+struct MyApp {
+    _state: String,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        MyApp::new()
+    }
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+    _Message1,
+}
+
+impl MyApp {
+    fn new() -> Self {
+        Self {
+            _state: String::new(),
+        }
+    }
+
+    fn update(&mut self, _message: Message) {
+        todo!()
+    }
+
+    fn view(&self) -> Element<Message> {
+        column!(text("Hello World!".to_string()),).into()
+    }
 }
 
 struct MyWidget;
 
 impl<Message, Renderer> Widget<Message, Theme, Renderer> for MyWidget
 where
-    Renderer: Renderer,
+    Renderer: iced::advanced::Renderer,
 {
     fn size(&self) -> Size<Length> {
         Size {
@@ -22,23 +52,42 @@ where
 
     fn layout(
         &self,
-        tree: &mut widget::Tree,
-        renderer: &Renderer,
-        limits: &layout::Limits,
+        _tree: &mut widget::Tree,
+        _renderer: &Renderer,
+        _limits: &layout::Limits,
     ) -> layout::Node {
-        
+        layout::Node::new(Size::new(100., 100.))
     }
 
     fn draw(
         &self,
-        tree: &widget::Tree,
+        _tree: &widget::Tree,
         renderer: &mut Renderer,
-        theme: &Theme,
-        style: &renderer::Style,
+        _theme: &Theme,
+        _style: &renderer::Style,
         layout: Layout<'_>,
-        cursor: mouse::Cursor,
-        viewport: &Rectangle,
+        _cursor: mouse::Cursor,
+        _viewport: &Rectangle,
     ) {
-        todo!()
+        renderer.fill_quad(
+            Quad{
+                bounds: layout.bounds(),
+                border: Border {
+                    color: Color::from_rgb(0.6, 0.8, 1.0),
+                    width: 1.0,
+                    radius: 10.0.into(),
+                },
+                shadow: Shadow::default(),
+            },
+            Color::from_rgb(0.0, 0.2, 0.4));
+    }
+}
+
+impl<'a, Message, Renderer> From<MyWidget> for Element<'a, Message, Theme, Renderer>
+where
+    Renderer: iced::advanced::Renderer,
+{
+    fn from(widget: MyWidget) -> Self {
+        Self::new(widget)
     }
 }
