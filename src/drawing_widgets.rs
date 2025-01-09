e92ede0d15087f7c745a2316c172f43b8d786544
administroot <1474668090@qq.com>
2025-01-09 15:05:58 +0800

    Drawing_widgets

---
+++ src/drawing_widgets.rs
@@ -0,0 +1,106 @@
+use iced::{
+    advanced::{
+        layout,
+        renderer::{self, Quad},
+        widget, Layout, Widget,
+    },
+    alignment, mouse,
+    widget::container,
+    Border, Color, Element, Length, Rectangle, Shadow, Size, Theme,
+};
+
+fn main() -> iced::Result {
+    iced::application("drawing widgets", MyApp::update, MyApp::view).run()
+}
+
+struct MyApp {
+    _state: String,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        MyApp::new()
+    }
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+    _Message1,
+}
+
+impl MyApp {
+    fn new() -> Self {
+        Self {
+            _state: String::new(),
+        }
+    }
+
+    fn update(&mut self, _message: Message) {
+        todo!()
+    }
+
+    fn view(&self) -> Element<Message> {
+        container(MyWidget)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .align_x(alignment::Horizontal::Center)
+            .align_y(alignment::Vertical::Center)
+            .into()
+    }
+}
+
+struct MyWidget;
+
+impl<Message, Renderer> Widget<Message, Theme, Renderer> for MyWidget
+where
+    Renderer: iced::advanced::Renderer,
+{
+    fn size(&self) -> Size<Length> {
+        Size {
+            width: Length::Shrink,
+            height: Length::Shrink,
+        }
+    }
+
+    fn layout(
+        &self,
+        _tree: &mut widget::Tree,
+        _renderer: &Renderer,
+        _limits: &layout::Limits,
+    ) -> layout::Node {
+        layout::Node::new(Size::new(100., 100.))
+    }
+
+    fn draw(
+        &self,
+        _tree: &widget::Tree,
+        renderer: &mut Renderer,
+        _theme: &Theme,
+        _style: &renderer::Style,
+        layout: Layout<'_>,
+        _cursor: mouse::Cursor,
+        _viewport: &Rectangle,
+    ) {
+        renderer.fill_quad(
+            Quad {
+                bounds: layout.bounds(),
+                border: Border {
+                    color: Color::from_rgb(0.6, 0.8, 1.0),
+                    width: 1.0,
+                    radius: 10.0.into(),
+                },
+                shadow: Shadow::default(),
+            },
+            Color::from_rgb(0.0, 0.2, 0.4),
+        );
+    }
+}
+
+impl<'a, Message, Renderer> From<MyWidget> for Element<'a, Message, Theme, Renderer>
+where
+    Renderer: iced::advanced::Renderer,
+{
+    fn from(widget: MyWidget) -> Self {
+        Self::new(widget)
+    }
+}
