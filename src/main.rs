c2add0e1251c067794af4b75f7696c255fb40183
administroot <1474668090@qq.com>
2025-01-12 13:23:10 +0800

    vault backup: 2025-01-12 13:23:09

--- src/main.rs
+++ src/main.rs
@@ -44,73 +44,16 @@ impl MyApp {
     }
 }
 
-struct MyWidgetInner;
+struct MyWidgetOuter<'a, Message, Renderer> {
+    inner_widget: Element<'a, Message, Theme, Renderer>
+}
 
-impl<Message, Renderer> Widget<Message, Theme, Renderer> for MyWidgetInner
-where
-    Renderer: iced::advanced::Renderer,
+impl<'a, Message, Renderer> MyWidgetOuter<'a, Message, Renderer> 
+where Renderer: iced::advanced::Renderer,
 {
-    fn size(&self) -> Size<Length> {
-        Size {
-            width: Length::Shrink,
-            height: Length::Shrink,
-        }
-    }
-
-    fn layout(
-        &self,
-        _tree: &mut Tree,
-        _renderer: &Renderer,
-        _limits: &layout::Limits,
-    ) -> layout::Node {
-        layout::Node::new(Size {
-            width: 100.0,
-            height: 100.0,
-        })
-    }
-
-    fn draw(
-        &self,
-        _state: &Tree,
-        renderer: &mut Renderer,
-        _theme: &Theme,
-        _style: &renderer::Style,
-        layout: Layout<'_>,
-        _cursor: mouse::Cursor,
-        _viewport: &Rectangle,
-    ) {
-        renderer.fill_quad(
-            Quad {
-                bounds: layout.bounds(),
-                border: Border {
-                    color: Color::from_rgb(0.6, 0.8, 1.0),
-                    width: 1.0,
-                    radius: 10.0.into(),
-                },
-                shadow: Shadow::default(),
-            },
-            Color::from_rgb(0.0, 0.2, 0.4),
-        );
-    }
-}
-
-impl<'a, Message, Renderer> From<MyWidgetInner> for Element<'a, Message, Theme, Renderer>
-where
-    Renderer: iced::advanced::Renderer,
-{
-    fn from(widget: MyWidgetInner) -> Self {
-        Self::new(widget)
-    }
-}
-
-struct MyWidgetOuter {
-    inner_widget: MyWidgetInner,
-}
-
-impl MyWidgetOuter {
-    fn new() -> Self {
+    fn new(inner_widget: Element<'a, Message, Theme, Renderer>) -> Self {
         Self {
-            inner_widget: MyWidgetInner,
+            inner_widget
         }
     }
 }
@@ -132,11 +75,7 @@ where
         renderer: &Renderer,
         limits: &layout::Limits,
     ) -> layout::Node {
-        let inner_widget = &self.inner_widget as &dyn Widget<Message, Theme, Renderer>;
-        let mut child_node = inner_widget.layout(tree, renderer, limits);
-        let size_of_this_node = Size::new(200., 200.);
-        child_node = child_node.align(Alignment::Center, Alignment::Center, size_of_this_node);
-        layout::Node::with_children(size_of_this_node, vec![child_node])
+        let mut child_node = self.inner_widget.
     }
 
     fn draw(
